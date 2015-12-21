9828fb2945291dfba3082f6eed1e9170778d390f
FantDev <andrey.i.kutkov@yandex.com>
2015-12-21 04:59:08 +0300

    first commit

---
+++ SampleIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleIOS
+//
+//  Created by ARDev on 12/21/15.
+//  Copyright © 2015 RY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleIOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleIOS
+//
+//  Created by ARDev on 12/21/15.
+//  Copyright © 2015 RY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
